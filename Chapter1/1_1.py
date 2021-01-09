7c11e0f1fd9b13e431c0b55eb2f08402cb904305
Ziven <shigaoyu615@gmail.com>
2021-01-09 13:36:49 +1100

    finish chapter 2

--- Chapter1/1_1.py
+++ Chapter1/1_1.py
@@ -1,21 +1,28 @@
 import collections
 
-Card = collections.namedtuple('Card',['rank','suit'])
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
 
 class FrenchDeck:
-    ranks = [str(n) for n in range(2,11)] + list('JQKA')
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
 
     def __init__(self):
-        self._cards = [Card(rank,suit) for rank in self.ranks for suit in self.suits]
-        
+        self._cards = [
+            Card(rank, suit) for rank in self.ranks for suit in self.suits
+        ]
+
     def __len__(self):
         return len(self._cards)
 
-    def __getitem__(self,position):
+    def __getitem__(self, position):
         return self._cards[position]
 
-beer_card = Card('7','diamonds')
+    def __setitem__(self, position, card):
+        self._cards[position] = card
+
+
+beer_card = Card('7', 'diamonds')
 print(beer_card)
 deck = FrenchDeck()
 print(len(deck))
@@ -23,10 +30,14 @@ print(deck[0])
 print(deck[1])
 from random import choice
 print(choice(deck))
-suit_value = dict(spades=3,hearts=2,diamonds=1,clubs=0)
+suit_value = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
 def spades_high(card):
     rank_value = FrenchDeck.ranks.index(card.rank)
-    return rank_value*len(suit_value) + suit_value[card.suit]
-a = sorted(deck,key=spades_high)
+    return rank_value * len(suit_value) + suit_value[card.suit]
+
+
+a = sorted(deck, key=spades_high)
 print(type(a))
 print(type(deck))
